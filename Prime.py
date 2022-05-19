8c32afd9ff5f162bad5fb3c1015c57f17e89093a
PrathyushaReddy1105 <18w91a1203@mriet.ac.in>
2022-05-20 01:22:46 +0530

    Check number is Prime or Not

---
+++ Prime.py
@@ -0,0 +1,12 @@
+n=int(input())
+count=0
+for i in range(1,n+1):
+
+    if n%i!=0:
+        continue
+    else:
+       count+=1
+if count<=2:
+    print(f"{n} is  a prime number")
+else:
+    print(f"{n} is not a prime number")
